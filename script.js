1885cb5bdb82d1d4ad5c4994f5d9e987bfe261c5
musalaandre <musalaandre@gmail.com>
2026-01-20 14:42:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Merci pour votre message !");
+});
